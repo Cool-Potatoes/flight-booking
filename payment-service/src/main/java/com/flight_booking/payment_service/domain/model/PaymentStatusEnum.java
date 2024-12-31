a7bbca907934106a176f4205de262f372f4f4888
k-jaehyun <wogus146@gmail.com>
2024-12-31 18:41:20 +0900

    feat: Payment PaymentStatusEnum 수정

--- payment-service/src/main/java/com/flight_booking/payment_service/domain/model/PaymentStatusEnum.java
+++ payment-service/src/main/java/com/flight_booking/payment_service/domain/model/PaymentStatusEnum.java
@@ -2,21 +2,11 @@ package com.flight_booking.payment_service.domain.model;
 
 public enum PaymentStatusEnum {
 
-  PENDING("PENDING"),         // 결제 대기중 (결제 요청됨, 처리 전)
-  IN_PROGRESS("IN_PROGRESS"), // 결제 진행 중 (결제 처리 중)
-  PAYED("PAYED"),             // 결제 완료
-  REFUND("REFUND"),           // 환불 완료
-  PAYED_FAIL("PAYED_FAIL"),   // 결제 실패
-  REFUND_FAIL("REFUND_FAIL"); // 환불 실패
-
-  private final String state;
-
-  PaymentStatusEnum(String state) {
-    this.state = state;
-  }
-
-  public String getStatus() {
-    return this.state;
-  }
+  PENDING,      // 결제 대기중 (결제 요청됨, 처리 전)
+  IN_PROGRESS,  // 결제 진행 중 (결제 처리 중)
+  PAYED,        // 결제 완료
+  REFUND,       // 환불 완료
+  PAYED_FAIL,   // 결제 실패
+  REFUND_FAIL;  // 환불 실패
 
 }
