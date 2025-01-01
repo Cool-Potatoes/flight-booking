7b92a26a9766a58fb1c3757c307ef48d46b987bd
k-jaehyun <wogus146@gmail.com>
2025-01-02 02:12:12 +0900

    fix: Payment delete시 deletedAt 필드 수정 추가

--- payment-service/src/main/java/com/flight_booking/payment_service/domain/model/Payment.java
+++ payment-service/src/main/java/com/flight_booking/payment_service/domain/model/Payment.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -42,6 +43,7 @@ public class Payment extends BaseEntity {
   }
 
   public void delete() {
+    this.deletedAt = LocalDateTime.now();
     this.isDeleted = true;
   }
 
