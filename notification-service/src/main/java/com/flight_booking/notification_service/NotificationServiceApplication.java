e09a9a350388340098a4d7e81f3d538706d01bfa
HanBeom98 <Carry9691@gmail.com>
2025-01-03 10:53:40 +0900

    feat: EnableAsync 어노테이션 추가

--- notification-service/src/main/java/com/flight_booking/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/flight_booking/notification_service/NotificationServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableAsync // 비동기 처리를 활성화
 @EnableJpaAuditing  // 감사(Auditing) 필드 활성화
 public class NotificationServiceApplication {
 
