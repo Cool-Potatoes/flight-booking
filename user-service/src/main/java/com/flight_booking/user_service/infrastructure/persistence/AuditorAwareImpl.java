344d3942b95dac6b9c7be7e062ba62a1dac5630c
Hujin0322 <hongyuj0322@naver.com>
2025-01-06 11:11:19 +0900

    refactor: JpaConfig 중복 수정 및 AuditorAwareImpl 리팩토링

--- user-service/src/main/java/com/flight_booking/user_service/infrastructure/persistence/AuditorAwareImpl.java
+++ user-service/src/main/java/com/flight_booking/user_service/infrastructure/persistence/AuditorAwareImpl.java
@@ -2,17 +2,22 @@ package com.flight_booking.user_service.infrastructure.persistence;
 
 import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
   @Override
   public Optional<String> getCurrentAuditor() {
-    // 현재 인증된 사용자 정보를 SecurityContext에서 가져옴
-    String currentUser = SecurityContextHolder.getContext().getAuthentication() != null ?
-        SecurityContextHolder.getContext().getAuthentication().getName() : "Anonymous";
+    // 인증된 사용자 정보
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-    // 인증되지 않은 사용자
-    return Optional.ofNullable(currentUser);
+    // 인증되지 않은 사용자일 경우 "SYSTEM" 또는 "AnonymousUser"
+    if (authentication == null || authentication.getPrincipal() == "anonymousUser") {
+      return Optional.of("SYSTEM");
+    }
+
+    // 인증된 사용자일 경우 이메일 반환
+    return Optional.ofNullable(authentication.getPrincipal().toString());
   }
 }
--- user-service/src/main/java/com/flight_booking/user_service/infrastructure/persistence/JpaConfig.java
+++ user-service/src/main/java/com/flight_booking/user_service/infrastructure/persistence/JpaConfig.java
@@ -1,16 +1,10 @@
 package com.flight_booking.user_service.infrastructure.persistence;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {
 
-  @Bean
-  public AuditorAware<String> auditorAware(){
-    return new AuditorAwareImpl();
-  }
 }
