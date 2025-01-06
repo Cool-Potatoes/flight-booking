a47b7057ee5d10ef8c52a14e20dfbd136ed1e328
k-jaehyun <wogus146@gmail.com>
2025-01-07 01:26:41 +0900

    feat: common 모듈 PaymentRequestDto 설정 (0.0.2-SNAPSHOT)

---
+++ common/src/main/java/com/flight_booking/common/application/dto/PaymentRequestDto.java
@@ -0,0 +1,12 @@
+package com.flight_booking.common.application.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.util.UUID;
+
+public record PaymentRequestDto(
+    @NotNull(message = "Booking ID cannot be null") UUID bookingId,
+    @NotNull(message = "Fare cannot be null") @Positive(message = "Fare must be positive") Integer fare) {
+
+}
+
