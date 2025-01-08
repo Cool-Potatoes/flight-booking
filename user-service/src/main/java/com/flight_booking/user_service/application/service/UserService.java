e8eea7883591c78e21cc7940241138a87b8b5cd7
Hujin0322 <hongyuj0322@naver.com>
2025-01-08 14:38:25 +0900

    feat: 관리자와 사용자 상세 조회 로직 분리 및 코드 리팩토링
    - 관리자: 모든 사용자 정보 조회 가능
    - 사용자: 본인 정보만 조회 가능

--- user-service/src/main/java/com/flight_booking/user_service/application/service/UserService.java
+++ user-service/src/main/java/com/flight_booking/user_service/application/service/UserService.java
@@ -1,9 +1,12 @@
 package com.flight_booking.user_service.application.service;
 
+import com.flight_booking.user_service.domain.model.Role;
 import com.flight_booking.user_service.domain.model.User;
 import com.flight_booking.user_service.domain.repository.UserRepository;
+import com.flight_booking.user_service.infrastructure.security.authentication.CustomUserDetails;
 import com.flight_booking.user_service.presentation.global.exception.ErrorCode;
 import com.flight_booking.user_service.presentation.global.exception.UserException;
+import com.flight_booking.user_service.presentation.request.UpdateRequest;
 import com.flight_booking.user_service.presentation.response.AdminUserDetailResponse;
 import com.flight_booking.user_service.presentation.response.PageResponse;
 import com.flight_booking.user_service.presentation.response.UserDetailResponse;
@@ -29,22 +32,40 @@ public class UserService {
     return PageResponse.from(users);
   }
 
-  // 관리자일 경우 모든 사용자 정보 제공
-  public AdminUserDetailResponse getAdminUserDetails(Long id) {
+  // 회원 상세 정보 조회
+  @Transactional(readOnly = true)
+  public Object getUserDetails(Long id, CustomUserDetails userDetails) {
     User user = getUser(id);
-    return AdminUserDetailResponse.fromEntity(user);
-  }
+    boolean isAdmin = isAdmin(userDetails);
 
-  // 사용자의 경우 본인 정보만 조회
-  public UserDetailResponse getUserDetails(Long id) {
-    User user = getUser(id);
+    // 관리자의 경우
+    if (isAdmin) {
+      return AdminUserDetailResponse.fromEntity(user);
+    }
+
+    // 사용자의 경우 본인 정보만 조회 가능
+    checkUser(userDetails, user);
     return UserDetailResponse.fromEntity(user);
   }
 
-  // 사용자 조회
+  // -----------------------------------------------------------------------------------------------
+
+  // 존재하는 사용자 확인
   private User getUser(Long id) {
     return userRepository.findById(id)
         .orElseThrow(() -> new UserException(ErrorCode.ACCESS_ONLY_SELF));
   }
 
+  // 관리자 권한 확인
+  private boolean isAdmin(CustomUserDetails userDetails) {
+    return userDetails.getAuthorities().stream()
+        .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
+  }
+
+  // 사용자 본인 확인
+  private void checkUser(CustomUserDetails userDetails, User user) {
+    if (!userDetails.getUsername().equals(user.getEmail())) {
+      throw new UserException(ErrorCode.ACCESS_ONLY_SELF);
+    }
+  }
 }
--- user-service/src/main/java/com/flight_booking/user_service/presentation/controller/UserController.java
+++ user-service/src/main/java/com/flight_booking/user_service/presentation/controller/UserController.java
@@ -3,18 +3,17 @@ package com.flight_booking.user_service.presentation.controller;
 import com.flight_booking.common.presentation.global.ApiResponse;
 import com.flight_booking.user_service.application.service.UserService;
 import com.flight_booking.user_service.infrastructure.security.authentication.CustomUserDetails;
-import com.flight_booking.user_service.presentation.global.exception.ErrorCode;
-import com.flight_booking.user_service.presentation.response.AdminUserDetailResponse;
+import com.flight_booking.user_service.presentation.request.UpdateRequest;
 import com.flight_booking.user_service.presentation.response.PageResponse;
-import com.flight_booking.user_service.presentation.response.UserDetailResponse;
 import com.flight_booking.user_service.presentation.response.UserListResponse;
-import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,32 +35,20 @@ public class UserController {
       @RequestParam(required = false) Boolean isBlocked,
       Pageable pageable) {
 
-    PageResponse<UserListResponse> response = userService.getUserList(email, name, role, isBlocked,
-        pageable);
+    PageResponse<UserListResponse> response = userService.getUserList(
+        email, name, role, isBlocked, pageable);
 
     return ApiResponse.ok(response, "회원 목록 조회 성공");
   }
 
   // 회원 상세 조회
   @GetMapping("/{id}")
-  public ApiResponse<?> getUserById(
+  public ApiResponse<?> getUserDetails(
       @PathVariable(name = "id") Long id,
       @AuthenticationPrincipal CustomUserDetails userDetails) {
 
-    // 관리자일 경우
-    if (userDetails.getAuthorities().equals("ADMIN")) {
-      AdminUserDetailResponse response = userService.getAdminUserDetails(id);
-      return ApiResponse.ok(response, "회원 상세 조회 성공");
-    }
-
-    // 사용자의 경우 본인 정보만 조회
-    if (!userDetails.getId().equals(id)) {
-      return ApiResponse.of(ErrorCode.ACCESS_ONLY_SELF.getHttpStatus(),
-          Collections.singletonList(ErrorCode.ACCESS_ONLY_SELF.getMessage()));
-    }
-
     // 사용자는 본인 정보만 제공
-    UserDetailResponse response = userService.getUserDetails(id);
+    Object response = userService.getUserDetails(id, userDetails);
     return ApiResponse.ok(response, "회원 상세 조회 성공");
   }
 }
