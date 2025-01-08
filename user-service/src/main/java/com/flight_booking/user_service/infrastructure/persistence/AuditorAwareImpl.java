adbc12d36f1d4ca02336d86bd1e3df566c00e163
Hujin0322 <hongyuj0322@naver.com>
2025-01-09 00:39:36 +0900

    fix: 인증된 사용자 정보 처리 방식 개선 및 타입 확인 추가

--- user-service/src/main/java/com/flight_booking/user_service/infrastructure/persistence/AuditorAwareImpl.java
+++ user-service/src/main/java/com/flight_booking/user_service/infrastructure/persistence/AuditorAwareImpl.java
@@ -2,15 +2,19 @@ package com.flight_booking.user_service.infrastructure.persistence;
 
 import com.flight_booking.user_service.infrastructure.security.authentication.CustomUserDetails;
 import java.util.Optional;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class AuditorAwareImpl implements AuditorAware<String> {
 
   @Override
+  @NonNull
   public Optional<String> getCurrentAuditor() {
     // 인증된 사용자 정보
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -20,9 +24,14 @@ public class AuditorAwareImpl implements AuditorAware<String> {
       return Optional.of("SYSTEM");
     }
 
-    CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-
-    // CustomUserDetails의 getUsername(email) 사용
-    return Optional.of(userDetails.getUsername());
+    // 타입 확인
+    Object principal = authentication.getPrincipal();
+    if (principal instanceof CustomUserDetails userDetails) {
+      return Optional.ofNullable(userDetails.getUsername());
+    } else {
+      // 예상한 타입이 아닌 경우 로그 기록
+      log.warn("Principal CustomUserDetails 인스턴스가 아닙니다: {}", principal);
+      return Optional.empty();
+    }
   }
 }
