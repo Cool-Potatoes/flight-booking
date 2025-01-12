af94df61001a9f9b1fceca8f6f25458ecbb62d99
k-jaehyun <wogus146@gmail.com>
2025-01-12 15:45:07 +0900

    fix: Booking @ComponentScan 범위 추가 및 KafkaSender로 방식 변경

--- booking-service/src/main/java/com/flight_booking/booking_service/BookingServiceApplication.java
+++ booking-service/src/main/java/com/flight_booking/booking_service/BookingServiceApplication.java
@@ -3,9 +3,11 @@ package com.flight_booking.booking_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan(basePackages = {"com.flight_booking.booking_service", "com.flight_booking.common"})
 public class BookingServiceApplication {
 
 	public static void main(String[] args) {
