732a1aabae64c74387bc0651871a7e8c92224cf8
Hujin0322 <hongyuj0322@naver.com>
2025-01-13 15:27:47 +0900

    chore: 불필요한 어노테이션 및 파일 제거

--- common/src/main/java/com/flight_booking/common/infrastructure/security/AuthenticationFilter.java
+++ common/src/main/java/com/flight_booking/common/infrastructure/security/AuthenticationFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.constraints.NotNull;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
