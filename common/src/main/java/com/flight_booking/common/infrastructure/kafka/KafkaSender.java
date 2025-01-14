a8b7ba4dec5c2c9808ae3260d2f3cb486281a0e9
k-jaehyun <wogus146@gmail.com>
2025-01-14 12:39:06 +0900

    fix: Common KafkaSender, KafkaSenderImpl 수정

--- common/src/main/java/com/flight_booking/common/infrastructure/kafka/KafkaSender.java
+++ common/src/main/java/com/flight_booking/common/infrastructure/kafka/KafkaSender.java
@@ -2,6 +2,6 @@ package com.flight_booking.common.infrastructure.kafka;
 
 public interface KafkaSender {
 
-  void sendApiResponseData(String topic, String key, Object data);
+  void sendApiResponseDataWithFrom(String topic, String key, Object data, String methodName, String className);
 
 }
