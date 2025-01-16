5c0b17b72e909d580e5a7dc2d7458ac3134d7759
HanBeom98 <Carry9691@gmail.com>
2025-01-16 13:04:31 +0900

    refactor: 테스트 서비스 유레카 비활성화

--- booking-service/src/test/java/com/flight_booking/booking_service/BookingServiceApplicationTests.java
+++ booking-service/src/test/java/com/flight_booking/booking_service/BookingServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.flight_booking.booking_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BookingServiceApplicationTests {
 
 	@Test
