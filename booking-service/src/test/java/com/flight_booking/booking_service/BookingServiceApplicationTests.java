295ec358bf111b15aaa8561ecb96a6a45af6c132
HanBeom98 <Carry9691@gmail.com>
2025-01-16 15:51:11 +0900

    fix: 롤백

--- booking-service/src/test/java/com/flight_booking/booking_service/BookingServiceApplicationTests.java
+++ booking-service/src/test/java/com/flight_booking/booking_service/BookingServiceApplicationTests.java
@@ -2,7 +2,7 @@ package com.flight_booking.booking_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
 class BookingServiceApplicationTests {
