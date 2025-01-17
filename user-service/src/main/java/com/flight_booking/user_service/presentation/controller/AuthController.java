13433e176b39338285e3df5170c327e3130a77ec
Hujin0322 <hongyuj0322@naver.com>
2025-01-18 00:51:23 +0900

    refactor: 리프레시 토큰 쿠키 이름 변경

--- user-service/src/main/java/com/flight_booking/user_service/presentation/controller/AuthController.java
+++ user-service/src/main/java/com/flight_booking/user_service/presentation/controller/AuthController.java
@@ -50,7 +50,7 @@ public class AuthController {
   // 토큰 재발급
   @PostMapping("/token")
   public ApiResponse<?> refreshToken(
-      @CookieValue(value = "REFRESH_TOKEN") String refreshToken,
+      @CookieValue(value = "RefreshToken") String refreshToken,
       @RequestHeader(value = "Authorization") String accessToken,
       HttpServletResponse response) {
     String newAccessToken = authService.renewTokens(refreshToken, accessToken, response);
@@ -60,7 +60,7 @@ public class AuthController {
   // 로그아웃
   @PostMapping("/logout")
   public ApiResponse<?> logout(
-      @CookieValue(value = "REFRESH_TOKEN") String refreshToken,
+      @CookieValue(value = "RefreshToken") String refreshToken,
       @RequestHeader(value = "Authorization") String accessToken,
       HttpServletResponse response) {
     authService.logout(refreshToken, accessToken, response);
