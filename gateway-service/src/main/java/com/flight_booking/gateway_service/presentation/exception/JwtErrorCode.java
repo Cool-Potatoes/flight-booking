7aba112df49b139890b90c8e75355f803e7c04ba
Hujin0322 <hongyuj0322@naver.com>
2025-01-19 17:15:42 +0900

    feat: 사용자 상태 확인 로직 필터에 추가 및 에러 코드 설정 추가

--- gateway-service/src/main/java/com/flight_booking/gateway_service/presentation/exception/JwtErrorCode.java
+++ gateway-service/src/main/java/com/flight_booking/gateway_service/presentation/exception/JwtErrorCode.java
@@ -1,4 +1,4 @@
-package com.flight_booking.gateway_service.util;
+package com.flight_booking.gateway_service.presentation.exception;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -16,7 +16,11 @@ public enum JwtErrorCode {
   EMAIL_EXTRACTION_FAILED(HttpServletResponse.SC_UNAUTHORIZED, "이메일 추출에 실패했습니다."),
   ROLE_EXTRACTION_FAILED(HttpServletResponse.SC_UNAUTHORIZED, "역할 추출에 실패했습니다."),
   INVALID_SECRET_KEY(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "SECRET_KEY 초기화에 실패했습니다."),
-  BLACKLISTED_TOKEN(HttpServletResponse.SC_UNAUTHORIZED, "해당 토큰은 블랙리스트에 포함되어 있습니다.");
+  BLACKLISTED_TOKEN(HttpServletResponse.SC_UNAUTHORIZED, "해당 토큰은 블랙리스트에 포함되어 있습니다."),
+  USER_BLOCKED(HttpServletResponse.SC_FORBIDDEN, "블락 처리된 회원입니다."),
+  USER_DELETED(HttpServletResponse.SC_FORBIDDEN, "탈퇴한 회원입니다."),
+  INVALID_TOKEN(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 토큰입니다."), // 추가된 메시지
+  TOKEN_VALIDATION_ERROR(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "토큰 검증 중 오류가 발생했습니다.");
 
   private final int statusCode;
   private final String message;
